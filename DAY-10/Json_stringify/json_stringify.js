bc02e1c481742147c353eff03d0d05d7b563db5a
Heli Gandhi <heligandhi4114@gmail.com>
2025-12-18 08:39:48 +0530

    Day 10 completed.

---
+++ DAY-10/Json_stringify/json_stringify.js
@@ -0,0 +1,38 @@
+// JSON
+// JavaScript Object Notation (JSON) is a standard text-based format for 
+// representing structured data based on JavaScript object syntax. 
+// It is commonly used for transmitting data in web applications (e.g., sending 
+// some data from the server to the client, so it can be displayed on a web page, 
+// or vice versa).
+
+// key:value is the building block.
+// { } contains an element.
+// [ ] contains an array of elements.
+// An element can have multiple key:value pairs.
+// Value can be a simple value like number or string etc., or an element or an 
+// array.
+// Elements in an Array could be accessed using index
+// Multiplekey:value pairs or elements are separated by comma
+
+
+// JSON Stringify
+// converts a JavaScript object or value to a JSON string
+
+let person = {
+    firstname : "Heli",
+    lastname : "Gandhi"
+};
+
+let data = JSON.stringify(person);
+console.log(data)
+
+// JSON parse
+// convert JSON Data into Javascript Object.
+
+let obj = `{
+    "firstname" : "Heli",
+    "lastname" : "Gandhi"
+}`;
+
+let o = JSON.parse(obj);
+console.log(o)
